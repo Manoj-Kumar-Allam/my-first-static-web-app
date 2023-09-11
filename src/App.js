64494ba3c1a54a62811f2d230e87af70aa4c9609
Manoj Kumar Allam <ma3932@nau.edu>
2023-09-11 16:24:55 -0700

    updated my static react app web application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import './App.css';
+import Lab1Image from './Lab1.png'; 
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <p>
+        I did my homework!
+      </p>
+      <img src={Lab1Image} width="1300" height="600" alt="Lab1"/>
+
+    </div>
+  );
 }
 
 export default App;
